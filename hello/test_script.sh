1e4dbad30b014708c675c54d71506b07f5105df4
chabibQNX <chabib@blackberry.com>
2023-09-19 06:15:28 -0400

    new test script

---
+++ hello/test_script.sh
@@ -0,0 +1,28 @@
+#!/bin/ksh
+
+if command -v /tmp/hello; then
+
+	echo "testing scenario 1, no arguments passed"
+	
+	if /tmp/hello | grep -q 'has to be run'; then
+  		echo "matched"
+  	else
+  		echo "no arguments scenario failed"
+  		exit 1
+	fi
+	
+	echo "testing scenario 2, arguments passed"
+	
+	if /tmp/hello 'hello world' | grep -q 'hello world'; then
+  		echo "matched"
+  	else
+  		echo "with arguments failed"
+  		exit 1
+	fi
+
+else
+	echo "File does not exist"
+	exit 1
+fi
+
+exit 0
